0a97e99b2045496c81103619c66795e7f212c347
agatapias <agata.piasecka@outlook.com>
2023-05-26 23:42:05 +0200

    working map of wroclaw

--- frontend/social-score/src/pages/DangerMap.js
+++ frontend/social-score/src/pages/DangerMap.js
@@ -1,28 +1,79 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { ComposableMap, Geographies, Geography, ZoomableGroup } from "react-simple-maps"
+import { scaleLinear } from "d3-scale";
 
 
-const geoUrl =
-  "https://raw.githubusercontent.com/ppatrzyk/polska-geojson/master/wojewodztwa/wojewodztwa-max.geojson"
+const geoUrl = "https://raw.githubusercontent.com/ppatrzyk/polska-geojson/master/miasta/wroclaw-max.geojson"
+  // "https://raw.githubusercontent.com/ppatrzyk/polska-geojson/master/wojewodztwa/wojewodztwa-max.geojson"
 
 export default function DangerMap({mapUrl}) {
-const [position, setPosition] = useState({coordinates:[0,0], zoom:1})
+const [position, setPosition] = useState({coordinates:[-15,51], zoom:12})
+
+  function handleZoomIn() {
+    setPosition({ ...position, zoom: position.zoom * 2 });
+  }
+
+  function handleZoomOut() {
+    setPosition({ ...position, zoom: position.zoom / 2 });
+  }
 
   const handleMoveEnd = (pos) => {
+    console.log(pos);
     setPosition(pos);
   }
 
+  window.onload = () => {
+    setPosition({coordinates:[-15,51], zoom: position.zoom })
+  };
+
+  const colorScale = scaleLinear().domain([0,100]).range(["a72bbp","0376db"])
+  const regions = []  // TODO: fill with data about crimes/dangerous people in given region of wroclaw
+
   return (
+    <div>
+      {/* TODO: doesn't work */}
+      <div className="controls">
+        <button onClick={handleZoomIn}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="3"
+          >
+            <line x1="12" y1="5" x2="12" y2="19" />
+            <line x1="5" y1="12" x2="19" y2="12" />
+          </svg>
+        </button>
+        <button onClick={handleZoomOut}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="3"
+          >
+            <line x1="5" y1="12" x2="19" y2="12" />
+          </svg>
+        </button>
+      </div>
     <ComposableMap projection="geoMercator" width={800} height={600}>
       <ZoomableGroup zoom={position.zoom} position={position.coordinates} onMoveEnd={handleMoveEnd}>
       <Geographies geography={geoUrl}>
         {({ geographies }) =>
-          geographies.map((geo) => (
-            <Geography key={geo.rsmKey} geography={geo} />
-          ))
+          geographies.map((geo) => {
+            const regionColors = regions.find((s) => s.id === geo.properties.ISO_A3)
+            return <Geography key={geo.rsmKey} geography={geo} fill={regionColors ? colorScale(regions['number']) : "#333"}/>
+          }
+          )
         }
       </Geographies>
       </ZoomableGroup>
     </ComposableMap>
+    
+    </div>
+    
   )
 }
