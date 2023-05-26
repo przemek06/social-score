ec6f4751d9550b30ff97b9bc5c02f676b02301ef
przemek06 <swiatprzemyslaw@gmail.com>
2023-05-26 22:50:15 +0200

    db setup

--- backend/social-score/index.js
+++ backend/social-score/index.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const cors = require("cors");
+const createTableQuery = require("./src/repo/create_tables");
+
+
+// initialize database
+createTableQuery()
 
 const app = express();
 
@@ -8,8 +13,6 @@ app.use(cors());
 app.get('/', function (req, res) {
   res.send('Hello World');
 })
-
-//
 
 app.use(function(err, req, res, next) {
   res.status(500).send(res.sentry);
